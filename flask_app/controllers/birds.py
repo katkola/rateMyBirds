83fbcc630e77e80653e85e9e44471ee659081cd0
katkola <katkola@gmail.com>
2023-04-20 02:17:23 -0400

    Home page, index login styled

--- flask_app/controllers/birds.py
+++ flask_app/controllers/birds.py
@@ -4,4 +4,8 @@ from flask_app.models.bird import bird
 
 @app.route('/')
 def home ():
+    return render_template ('index.html')
+
+@app.route('/profile')
+def userprofile():
     return render_template ('profile.html')
