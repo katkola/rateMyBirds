80c425f682545f21b6bb0f7d3f7f49d7626d5e1f
Quintilius <112987748+QTice@users.noreply.github.com>
2023-04-23 18:45:19 -0600

    Commit edit software

--- flask_app/controllers/birds.py
+++ flask_app/controllers/birds.py
@@ -1,32 +1,90 @@
 from flask_app import app
 from flask import render_template,redirect,request,session,flash
 from flask_app.models.bird import Bird
+from flask_app.models.user import User
+from flask_app.models.rating import Rating
 
-@app.route('/')
-def index():
-    return render_template("index.html")
 
 @app.route('/bird/add', methods=["POST"])
 def create_bird():
     is_valid_bird = Bird.validate_bird(request.form)
     if not is_valid_bird:
-        return redirect('/')
+        return redirect('/birds/new')
 
     data = {
         'species': request.form['species'],
-        'user_id': session['user_id']
+        'description': request.form['description'],
+        'user_id': session['user_id'],
+        'image_url': Bird.get_bird_image(request.form['description'])
     }
     
-    Bird.save_bird(data)
-    return redirect("/")
+    Bird.save(data)
+    return redirect("/dashboard")
 
-@app.route('/birds/view')
-def one_bird():
-    return render_template('oneBird.html')
+@app.route('/rating/add', methods=["POST"] )
+def create_rating():
+    if 'user_id' not in session:
+        return redirect('/')
+    data = {
+        'value': request.form['rating_value'],
+        'bird_id': request.form['bird_id'],
+        'user_id': session['user_id']
+    }
+    Rating.save(data)
+    return('/birds/single/<bird_id>')
 
-@app.route('/profile')
-def profile():
+
+@app.route('/birds/single/<int:bird_id>')
+def one(bird_id):
+    data ={
+        "id":bird_id
+    }
+    bird=Bird.get_one(data)
+    return render_template("oneBird.html", bird=bird)
+
+@app.route('/birds/new')
+def bird_form():
+    if 'user_id' not in session:
+        return redirect('/')
     data = {
         "id": session['user_id']
     }
-    return render_template('profile.html',user=User.get_one(data),birds=Bird.get_by_user(data))
+    user = User.get_one(data)
+    return render_template('newBirdForm.html', user=user)
+
+
+@app.route("/dashboard")
+def dashboard():
+    if 'user_id' not in session:
+        return redirect('/')
+    data = {
+        "id": session['user_id']
+    }
+    user = User.get_one(data)
+
+    return render_template("dashboard.html", birds=Bird.get_all())
+
+
+@app.route('/edit/<int:bird_id>')
+def edit(bird_id):
+    print(bird_id)
+    data ={ 
+
+        "id": bird_id,
+        "species": 'species',
+        "description": 'description'
+    }
+    Jane=Bird.get_one(data)
+    print(Jane)
+    return render_template("edit.html",bird=Bird.get_one(data))
+
+
+@app.route("/bird/update/<int:bird_id>", methods=["POST"])
+def Update_bird(bird_id):
+    data = {
+          "id": bird_id,
+        "species": request.form['species'],
+        "description": request.form['description']
+    }
+    bird=Bird.update(data)
+    return redirect("/dashboard")
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -4,37 +4,28 @@ import re
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
 
 
-
-
 class User:
-    db_name = 'ratebirds'
-    def __init__(self,db_data):
-        self.id = db_data['id']
-        self.first_name = db_data['first_name']
-        self.last_name = db_data['last_name']
-        self.email = db_data['email']
-        self.password = db_data['password']
-        self.created_at = db_data['created_at']
-        self.updated_at = db_data['updated_at']
-        self.birds = []
+    db_name = 'birds-schema'
+    def __init__(self,data):
+        self.id = data['id']
+        self.firstname = data['firstname']
+        self.lastname = data['lastname']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        # self.birds = []
+        # birds=[]
+        # ratings=[]
 
     @classmethod
     def save(cls,data):
-        query = "INSERT INTO users (first_name,last_name,email,password,created_at,updated_at) VALUES(%(first_name)s,%(last_name)s,%(email)s,%(password)s,NOW(),NOW())"
+        query = "INSERT INTO users (firstname,lastname,email,password,created_at,updated_at) VALUES(%(firstname)s,%(lastname)s,%(email)s,%(password)s,NOW(),NOW())"
         return connectToMySQL(cls.db_name).query_db(query,data)
 
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
-        results = connectToMySQL(cls.db_name).query_db(query)
-        users = []
-        for user in results:
-            users.append(cls(user))
-        return users
-    
-    @classmethod
-    def get_by_user(cls, data):
-        query = "SELECT * FROM birds WHERE birds.user_id = %(id)s;"
         results = connectToMySQL(cls.db_name).query_db(query)
         users = []
         for user in results:
@@ -60,20 +51,20 @@ class User:
     @staticmethod
     def validate_user(user):
         is_valid = True
-        if len(user['first_name']) < 2:
+        if len(user['firstname']) < 2:
             is_valid = False
-            flash("First name must be at least 3 characters.","register")
-        if len(user['last_name']) < 2:
+            flash("First name must be at least 3 characters","user")
+        if len(user['lastname']) < 2:
             is_valid = False
-            flash("Last name must be at least 3 characters.","register")
+            flash("Last name must be at least 3 characters","user")
         if not EMAIL_REGEX.match(user['email']):
             is_valid = False
-            flash("Invalid Email Address.","register")
+            flash("Invalid Email Address","user")
         if len(user['password']) < 8:
             is_valid = False
-            flash("Password must be at least 8 characters.","register")
+            flash("Password must be at least 8 characters","user")
         if user['password'] != user['confirm']:
             is_valid = False
-            flash("Passwords do not match!","register")
+            flash("Passwords do not match!","user")
 
         return is_valid
