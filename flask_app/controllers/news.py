0fff56081773e3f53d746f943fb372e1061bf7c6
Kat Hogan <32649279+katkola@users.noreply.github.com>
2023-04-25 17:00:57 -0400

    bird news api

--- flask_app/controllers/news.py
+++ flask_app/controllers/news.py
@@ -1,11 +1,22 @@
 from flask_app import app
 from flask import render_template,redirect,request,session,flash, url_for
-
-from flask_app import News
+from datetime import datetime, timedelta
+from flask_app.config.news_config import api_key
+from newsapi import NewsApiClient
+newsapi = NewsApiClient(api_key='0b45717f6a55487f8f0078b47e62751d')
+import json
+import requests
 
 @app.route('/news')
 def news():
     news = []
-    news = News.get_bird_headlines()
+    month_ago = datetime.now() - timedelta(days=30)
+    print(month_ago)
+    formated_date = str(month_ago.isoformat())[:10]
+    top_headlines = newsapi.get_everything(q='bird', 
+    from_param=formated_date,
+    page_size=5
+    )
+    articles = top_headlines['articles']
 
-    return render_template('news.html', news=news)
+    return render_template('news.html', articles=articles)
--- flask_app/models/newsmodel.py
+++ flask_app/models/newsmodel.py
@@ -1,43 +1,23 @@
-import requests
+from flask_app import app
+from flask import render_template,redirect,request,session,flash, url_for
+from datetime import datetime, timedelta
 from flask_app.config.news_config import api_key
+from newsapi import NewsApiClient
 newsapi = NewsApiClient(api_key='0b45717f6a55487f8f0078b47e62751d')
+import json
+import requests
 
 class News:
-    def __init__(self,data):
-    self.title= title
-    self.description = description
-    self.linke = linke
+
 
     @staticmethod
     def get_bird_headlines():
-        top_headlines = newsapi.get_top_headlines(q='birds',
-        sources='bbc-news,national-geographic',
-        category='nature,environment,climate',
-        language='en',country='us')
-        console.log(top_headlines)
-
-        articles = []
-
-        for article in top_headlines['articles']:
-            data= {
-                'title': article['title'],
-                'description': article['description'],
-                'linke': article['url']
-            }
-            articles.append(cls(data))
+        news = []
+        month_ago = datetime.now() - timedelta(days=30)
+        print(month_ago)
+        formated_date = str(month_ago.isoformat())[:10]
+        top_headlines = newsapi.get_everything(q='bird', 
+            from_param=formated_date,
+            page_size=5)
+        articles = top_headlines['articles']
         return articles
-
-
-
-    @staticmethod
-    def get_bird_news():
-        url = "https://newsapi.org/v2/top-headlines?q=birds"
-        parameters = {
-            "query": description,
-            "client_id": api_key
-        }
-        response = requests.get(url, params=parameters)
-        print(response.json())
-        for article in results.artiles:
-
-        return json.loads(response.content)['urls']['raw']
